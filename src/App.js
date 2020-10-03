fd46484111213a13ed265bedf4825325255554e8
Alyona <alyona.rodin@gmail.com>
2020-10-02 23:24:27 -0400

    icons

--- src/App.js
+++ src/App.js
@@ -6,18 +6,18 @@ import Header from './Header'
 
 const H1=styled.h1`
     position: absolute;
-    top: 50%;
+    top: 55%;
     left: 50%;
 
     transform: translate(-50%, -50%);
     width: 90%;
     text-align: center;
-    color:#663399;
-    font-size:30px;
-    font-weight: 700;
-    font-family: "Lato", sans-serif;
-    letter-spacing: 2px;
-    word-spacing: 10px;
+    color:#4b4a5e;
+    font-size:20px;
+    font-weight: 500;
+    font-family: Helvetica;
+    letter-spacing: 1px;
+    word-spacing: 5px;
 `
 const Part1=styled.div`
     height: 100vh;
@@ -33,6 +33,7 @@ height: 80vh;
 display: flex;
 align-items:center;
 justify-content:center;
+// background-color:#24292e;
 // background: linear-gradient(90deg, #fbfbfb 31px, transparent 1%) center, linear-gradient(#fbfbfb 31px, transparent 1%) center, #24292e;
 // background-size: 32px 32px;
 `
@@ -86,7 +87,7 @@ function App() {
       </Part2>
       <Part3>
         <Hr/>
-        <h5>© 2020 D3 React.</h5>
+        <h5>© 2020 D3 React</h5>
       </Part3>
     </div>
   );
--- src/components/Categories.js
+++ src/components/Categories.js
@@ -5,13 +5,13 @@ import './Categories.css'
 
 const B = styled.div`
     align-items:center;
-    width: 90px;
-    height: 90px;
+    width: 145px;
+    height: 105px;
     margin:auto;
     cursor: pointer;
     padding:1px;
-    border-radius: 50%;
-    border: 0.05px solid #663399;
+    border-radius: 2%;
+    border: 0.05px solid #4B4A5E;
     @media screen and (max-width: 489px) {
       padding:5px;
             } 
@@ -24,65 +24,82 @@ function Categories() {
 
     <Tilt>
       <div style={{ height: '100px', width:'100px', marginLeft:'50px',paddingBottom: '10px' , fontWeight: '300'}}>
-          <B><p>Bar</p></B> 
+          <B><svg width="157" height="99" viewBox="0 0 317 179" fill="none" xmlns="http://www.w3.org/2000/svg">
+
+<rect x="109.5" y="45.5" width="12" height="87" fill="#4B4A5E" stroke="#4B4A5E"/>
+<rect x="133.5" y="56.5" width="12" height="76" fill="#4B4A5E" stroke="#4B4A5E"/>
+<rect x="157.5" y="74.5" width="12" height="58" fill="#4B4A5E" stroke="#4B4A5E"/>
+<rect x="180.5" y="61.5" width="12" height="71" fill="#4B4A5E" stroke="#4B4A5E"/>
+</svg>
+          
+          
+          </B> 
       </div>
     </Tilt>
     
   
     <Tilt>
       <div style={{ height: '100px', width:'100px', marginLeft:'50px' ,paddingBottom: '10px'}}>
-      <B><p>Line</p></B> 
+      <B ><svg width="157" height="99" viewBox="0 0 317 179" fill="none" xmlns="http://www.w3.org/2000/svg">
+
+<path d="M78 126L109.636 94.5893H142.364L162 69.4608L193 108.824L238 53" stroke="#4B4A5E" stroke-width="4"/>
+</svg></B> 
           
       </div>
     </Tilt>
     <Tilt>
       <div style={{ height: '100px', width:'100px', marginLeft:'50px' ,paddingBottom: '10px'}}>
-      <B><p>Stack</p></B> 
+      <B><svg width="157" height="99" viewBox="0 0 317 179" fill="none" xmlns="http://www.w3.org/2000/svg">
+
+<circle cx="131" cy="100" r="31.5" fill="#4B4A5E" stroke="#4B4A5E"/>
+<circle cx="185.5" cy="68.5" r="22" fill="#4B4A5E" stroke="#4B4A5E"/>
+<circle cx="181.5" cy="108.5" r="8" fill="#4B4A5E" stroke="#4B4A5E"/>
+</svg></B> 
       </div>
     </Tilt>
     <Tilt>
       <div style={{ height: '100px', width:'100px', marginLeft:'50px',paddingBottom: '10px'}}>
-      <B><p>Pie</p></B> 
+      <B></B> 
       </div>
     </Tilt>
     <Tilt>
       <div style={{ height: '100px', width:'100px', marginLeft:'50px',paddingBottom: '10px' }}>
-          <B><p>Chord</p></B> 
+          <B></B> 
       </div>
     </Tilt>
     <Tilt>
       <div style={{ height: '100px', width:'100px', marginLeft:'50px' ,paddingBottom: '10px'}}>
-          <B><p>Chord</p></B> 
+          <B></B> 
       </div>
     </Tilt>
     <Tilt>
       <div style={{ height: '100px', width:'100px', marginLeft:'50px',paddingBottom: '10px' }}>
-          <B><p>Chord</p></B> 
+          <B></B> 
       </div>
     </Tilt>
     <Tilt>
       <div style={{ height: '100px', width:'100px', marginLeft:'50px',paddingBottom: '10px' }}>
-          <B><p>Chord</p></B> 
+          <B></B> 
       </div>
     </Tilt>
     <Tilt>
       <div style={{ height: '100px', width:'100px', marginLeft:'50px',paddingBottom: '10px' }}>
-          <B><p>Chord</p></B> 
+          <B></B> 
       </div>
     </Tilt>
     <Tilt>
       <div style={{ height: '100px', width:'100px', marginLeft:'50px',paddingBottom: '10px' }}>
-          <B><p>Chord</p></B> 
+          <B></B> 
       </div>
     </Tilt>
     <Tilt>
       <div style={{ height: '100px', width:'100px', marginLeft:'50px',paddingBottom: '10px' }}>
-          <B><p>Chord</p></B> 
+          <B></B> 
       </div>
     </Tilt>
     <Tilt>
       <div style={{ height: '100px', width:'100px', marginLeft:'50px',paddingBottom: '10px' }}>
-          <B><p>Chord</p></B> 
+          <B></B> 
       </div>
     </Tilt>
     </>
